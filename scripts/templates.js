c999f5227ac004a6b1ea53d14044734c25232010
stefanstraeter <stefan.straeter@gmail.com>
2025-12-18 15:15:18 +0100

    fix: onclick buttons and navigation

--- scripts/templates.js
+++ scripts/templates.js
@@ -19,66 +19,58 @@ function showNotFoundMessageHtml() {
   return `<p class="text-white">No Pokémon found.</p>`
 }
 
-
-function pokemonModalHtml(data, typesHtml, abilitiesHtml, statsHtml, currentId, isFirstPokemon) {
+function pokemonModalHtml(data) {
   return `
-    <dialog class="modal__content">
-      <button id="closeModalBtn" class="modal__close">&times;</button>
+    <div class="modal__content">
+      <button onclick="pokemonModal.close()" class="modal__close">&times;</button>
+      
       <div class="modal__header">
         <h2 class="modal__number">#${data.id}</h2>
         <h1 class="modal__name">${capitalize(data.name)}</h1>
       </div>
+
       <div class="modal__body">
-      <div>
-       <img src="${data.image}" class="modal__image" alt="${data.name}" />
-        <div class="modal__name-row">
-            <button id="prevPokemonBtn" class="modal__nav-btn" data-id="${currentId - 1}" 
-              ${isFirstPokemon ? 'disabled' : ''}> 
-              <i class="fa-solid fa-angle-left fa-xl"></i>
+        <div class="modal__image-container">
+          <img src="${data.image}" class="modal__image" alt="${data.name}" />
+          <div class="modal__name-row">
+            <button id="prevPokemonBtn" class="modal__nav-btn" onclick="openPokemonModal(${data.id - 1})"> 
+                <i class="fa-solid fa-angle-left fa-xl"></i>
             </button>
-            <button id="nextPokemonBtn" class="modal__nav-btn" data-id="${currentId + 1}">
-            <i class="fa-solid fa-angle-right fa-xl"></i>
+            <button class="modal__nav-btn" onclick="openPokemonModal(${data.id + 1})">
+                <i class="fa-solid fa-angle-right fa-xl"></i>
             </button>
+          </div>
         </div>
-      </div>
        
         <div class="modal__panel">
           <div class="modal__tabs">
-            <button class="modal__tab modal__tab--active" data-tab="overview">Overview</button>
-            <button class="modal__tab" data-tab="stats">Stats</button>
-        </div>   
-            <div id="tabOverview" class="modal__tab-content modal__tab-content--active">
+            <button class="modal__tab modal__tab--active" onclick="switchTab('overview', this)">Overview</button>
+            <button class="modal__tab" onclick="switchTab('stats', this)">Stats</button>
+          </div>   
+
+          <div id="tabOverview" class="modal__tab-content modal__tab-content--active">
               <h3 class="modal__subtitle">Types</h3>
-              <div class="modal__types">
-                 ${typesHtml}
-              </div>
+              <div class="modal__types">${renderTypesForTemplate(data.types)}</div>
+              
               <h3 class="modal__subtitle modal__subtitle--spaced">Size & Weight</h3>
               <ul class="modal__size-weight">
-                  <li class="modal__size-weight-item">
-                     <span class="modal__label">Height:</span> 
-                     <span class="modal__value">${(data.height / 10).toFixed(1)} m</span>
-                  </li>
-                  <li class="modal__size-weight-item">
-                     <span class="modal__label">Weight:</span> 
-                     <span class="modal__value">${(data.weight / 10).toFixed(1)} kg</span>
-                  </li>
+                  <li><span class="modal__label">Height:</span> ${(data.height / 10).toFixed(1)} m</li>
+                  <li><span class="modal__label">Weight:</span> ${(data.weight / 10).toFixed(1)} kg</li>
               </ul>
+
               <h3 class="modal__subtitle modal__subtitle--spaced">Abilities</h3>
-              <ul id="modalPokemonAbilities" class="modal__abilities">
-                 ${abilitiesHtml}
-              </ul>
-        </div>
-        <div id="tabStats" class="modal__tab-content">
-          <h3 class="modal__subtitle">Stats</h3>
-          <ul id="modalPokemonStats" class="modal__stats">
-            ${statsHtml}
-          </ul>
+              <ul class="modal__abilities">${renderAbilitiesForTemplate(data.abilities)}</ul>
+          </div>
+
+          <div id="tabStats" class="modal__tab-content">
+            <h3 class="modal__subtitle">Stats</h3>
+            <ul id="modalPokemonStats" class="modal__stats">
+              ${renderStatsForTemplate(data.stats)}
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  </dialog>
-  `;
+    </div>`;
 }
-
 
 
